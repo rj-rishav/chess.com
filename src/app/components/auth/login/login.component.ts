c9650ca60e60b0d56552fe399a6f27751c21b36b
rj-rishav <chatwithme146@gmail.com>
2024-11-04 20:05:07 +0530

    Fix Login and Signup functionality

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-login',
@@ -10,11 +13,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   signinForm: FormGroup = new FormGroup({
     email: new FormControl(''),
     password: new FormControl('')
   });
+
+  constructor(private authService: AuthService, private cookieService: CookieService) {}
+
+
   onSubmit() {
-    console.log(this.signinForm.value);
+    const { email, password } = this.signinForm.value;
+    this.authService.signIn(email, password).then(
+      response => {
+        this.cookieService.set('refreshToken', response._tokenResponse.refreshToken);
+        console.log('Login successful', response);
+      }
+    ).catch(
+      error => {
+        console.error('Login failed', error);
+      }
+    );
   }
 }
--- src/app/components/auth/signin/signin.component.ts
+++ src/app/components/auth/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-signin',
@@ -16,17 +17,19 @@ export class SigninComponent {
     password: new FormControl('')
   });
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private cookieService: CookieService) {}
 
   onSubmit() {
     const { email, password } = this.signinForm.value;
     this.authService.signUp(email, password).then(
       response => {
-        console.log('Signin successful', response);
+        this.cookieService.set('refreshToken', response.refreshToken);
+        this.cookieService.set('accessToken', response.accessToken);
+        console.log('Signup successful', response);
       }
     ).catch(
       error => {
-        console.error('Signin failed', error);
+        console.log('Signup failed', error);
       }
     );
   }
